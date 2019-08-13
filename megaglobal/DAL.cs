435f6675645fa8e870adbdb5ac329f86709ebcdb
prabaharanh <prabaharan.haridass@outlook.com>
2019-08-14 09:50:52 +1200

    dal created

--- megaglobal/DAL.cs
+++ megaglobal/DAL.cs
@@ -1,12 +1,30 @@
 using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 
 namespace megaglobal
 {
   public class DAL
+
+
   {
- 
+
+    string connectionstring = ConfigurationManager.ConnectionStrings["dblogindemo"].ConnectionString;
+    SqlConnection connection;
+    public void userRegister(string firstname, string username, string password)
+    {
+      connection = new SqlConnection(connectionstring);
+      connection.Open();
+      string query = "insert into tbl_user values('" + username + "','" + password + "')";
+
+      SqlCommand command = new SqlCommand(query, connection);
+
+      command.ExecuteNonQuery();
+
+      connection.Close();
+    }
   }
 }
